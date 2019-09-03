e083ea137c4c96c3ea299061f698d3e2740dbbbb
Muhammad Angga Muttaqien <muha.muttaqien@gmail.com>
2019-09-03 17:15:56 +0900

    implemented cycleGAN in pytorch.

--- research/ai-python-programming/coursera-deep-learning-with-PyTorch/cyclegan/models.py
+++ research/ai-python-programming/coursera-deep-learning-with-PyTorch/cyclegan/models.py
@@ -11,7 +11,80 @@ def weights_init_normal(m):
     elif classname.find("BatchNorm2d") != -1:
         torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
         torch.nn.init.constant_(m.bias.data, 0.0)
+
+# -----------------------------
+# Generator
+# -----------------------------
+
+class ResidualBlock(nn.Module):
+    
+    def __init__(self, in_features):
+        super(ResidualBlock, self).__init__()
         
+        self.block = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_features, in_features, 3),
+            nn.InstanceNorm2d(in_features),
+            nn.ReLU(inplace=True),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_features, in_features, 3),
+            nn.InstanceNorm2d(in_features),
+        )
+        
+    def forward(self, x):
+        return x + self.block(x)
+
+class GeneratorResNet(nn.Module):
+    
+    def __init__(self, input_shape, num_residual_blocks):
+        super(GeneratorResNet, self).__init__()
+        
+        channels = input_shape[0]
+        
+        # initial convolution block
+        out_features = 64
+        model = [
+            nn.ReflectionPad2d(channels),
+            nn.Conv2d(channels, out_features, 7),
+            nn.InstanceNorm2d(out_features),
+            nn.ReLU(inplace=True),
+        ]
+        in_features = out_features
+        
+        # downsampling
+        for _ in range(2):
+            out_features *= 2
+            model += [
+                nn.Conv2d(in_features, out_features, 3, stride=2, padding=1),
+                nn.InstanceNorm2d(out_features),
+                nn.ReLU(inplace=True),
+            ]
+            in_features = out_features
+            
+        # residual blocks
+        for _ in range(num_residual_blocks):
+            model += [ResidualBlock(out_features)]
+            
+        # upsampling
+        for _ in range(2):
+            out_features //= 2
+            model += [
+                nn.Upsample(scale_factor=2),
+                nn.Conv2d(in_features, out_features, 3, stride=1, padding=1),
+                nn.InstanceNorm2d(out_features),
+                nn.ReLU(inplace=True),
+            ]
+            in_features = out_features
+            
+        # output layer
+        model += [nn.ReflectionPad2d(channels), nn.Conv2d(out_features, channels, 7), nn.Tanh()]
+        
+        self.model = nn.Sequential(*model)
+        
+    def forward(self, x):
+        return self.model(x)
+
+
 # -----------------------------
 # Discriminator
 # -----------------------------
