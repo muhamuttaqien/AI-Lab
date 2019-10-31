2008cdba79b2bb94d6f93196fb59da0a948094cb
Muhammad Angga Muttaqien <muha.muttaqien@gmail.com>
2019-10-31 14:23:46 +0900

    initialized transformer network.

--- research/deep-learning/sequence-learning/pytorch/transformer/Layers.py
+++ research/deep-learning/sequence-learning/pytorch/transformer/Layers.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from Sublayers import Norm, MultiHeadSelfAttention, FeedForward
+
 class EncoderLayer(nn.Module):
     
     def __init__(self, d_model, heads, dropout=0.1):
