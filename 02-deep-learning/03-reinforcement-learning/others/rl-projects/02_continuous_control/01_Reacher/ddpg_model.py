019961304f8980f8cec790a435963ac2139d92f5
Muhammad Angga Muttaqien <muha.muttaqien@gmail.com>
2020-02-29 00:15:36 +0900

    [RL][fix] Adjust parameters of DDPG algorithm for robotic arms

--- 02-deep-learning/03-reinforcement-learning/others/rl-projects/02_continuous_control/01_Reacher/ddpg_model.py
+++ 02-deep-learning/03-reinforcement-learning/others/rl-projects/02_continuous_control/01_Reacher/ddpg_model.py
@@ -24,19 +24,26 @@ class PolicyNetwork(nn.Module):
         self.state_size = state_size
         self.action_size = action_size
         
-        self.fc1_linear = nn.Linear(self.state_size, 256)
-        self.fc2_linear = nn.Linear(256, self.action_size)
+        self.normalizer = nn.BatchNorm1d(self.state_size)
+        
+        self.fc1_linear = nn.Linear(self.state_size, 512)
+        self.fc2_linear = nn.Linear(512, 256)
+        self.fc3_linear = nn.Linear(256, self.action_size)
         
         self.reset_parameters()
         
     def reset_parameters(self):
         self.fc1_linear.weight.data.uniform_(*hidden_init(self.fc1_linear))
-        self.fc2_linear.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc2_linear.weight.data.uniform_(*hidden_init(self.fc2_linear))
+        self.fc3_linear.weight.data.uniform_(-3e-3, 3e-3)
         
     def forward(self, state):
         
-        x = F.relu(self.fc1_linear(state))
-        x = torch.tanh(self.fc2_linear(x))
+        x = self.normalizer(state)
+
+        x = F.relu(self.fc1_linear(x))
+        x = F.relu(self.fc2_linear(x))
+        x = torch.tanh(self.fc3_linear(x))
         
         return x
     
@@ -51,26 +58,32 @@ class ValueNetwork(nn.Module):
         
         self.seed = torch.manual_seed(seed)
         
-        self.fcs1_linear = nn.Linear(state_size, 256)
-        self.fc2_linear = nn.Linear(256+action_size, 256)
-        self.fc3_linear = nn.Linear(256, 128)
-        self.fc4_linear = nn.Linear(128, 1)
+        self.state_size = state_size
+        self.action_size = action_size
+        
+        self.dropout = nn.Dropout(p=0.2)
+        self.normalizer = nn.BatchNorm1d(self.state_size)
+        
+        self.fcs1_linear = nn.Linear(self.state_size, 512)
+        self.fc2_linear = nn.Linear(512+self.action_size, 256)
+        self.fc3_linear = nn.Linear(256, 1)
         
         self.reset_parameters()
         
     def reset_parameters(self):
         self.fcs1_linear.weight.data.uniform_(*hidden_init(self.fcs1_linear))
         self.fc2_linear.weight.data.uniform_(*hidden_init(self.fc2_linear))
-        self.fc3_linear.weight.data.uniform_(*hidden_init(self.fc3_linear))
-        self.fc4_linear.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc3_linear.weight.data.uniform_(-3e-3, 3e-3)
         
     def forward(self, state, action):
         
-        xs = F.leaky_relu(self.fcs1_linear(state))
+        x = self.normalizer(state)
+        
+        xs = F.leaky_relu(self.fcs1_linear(x))
         x = torch.cat((xs, action), dim=1)
         x = F.leaky_relu(self.fc2_linear(x))
-        x = F.leaky_relu(self.fc3_linear(x))
-        Qsa = self.fc4_linear(x)
+        x = self.dropout(x)
+        Qsa = self.fc3_linear(x)
         
         return Qsa
     
