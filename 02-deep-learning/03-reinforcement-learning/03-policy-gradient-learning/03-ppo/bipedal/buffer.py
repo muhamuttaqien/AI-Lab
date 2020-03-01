d48f59eb0ed9e0e39e77786eed38b8d3dd7d3f13
Muhammad Angga Muttaqien <muha.muttaqien@gmail.com>
2020-03-01 16:04:37 +0900

    [RL][feat] Implement PPO algorithm

---
+++ 02-deep-learning/03-reinforcement-learning/03-policy-gradient-learning/03-ppo/bipedal/buffer.py
@@ -0,0 +1,11 @@
+class BasicBuffer(object):
+    
+    def __init__(self):
+        
+        self.states = []; self.actions = []; self.rewards = []; self.dones = []
+        self.log_probs = []
+        
+    def clear_memory(self):
+        del self.states[:]; del self.actions[:]; del self.rewards[:]; del self.dones[:]
+        del self.log_probs[:]
+        
---
+++ 02-deep-learning/03-reinforcement-learning/03-policy-gradient-learning/03-ppo/bipedal/model.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import MultivariateNormal
+
+class PolicyNetwork(nn.Module):
+    """Policy (Actor) Network."""
+    
+    def __init__(self, state_size, action_size, action_std, seed):
+        """Initialize parameters and build model."""
+        
+        super(PolicyNetwork, self).__init__()
+        
+        self.seed = torch.manual_seed(seed)
+        
+        self.state_size = state_size
+        self.action_size = action_size
+        self.action_std = action_std
+        
+        self.policy_layer =  nn.Sequential(nn.Linear(self.state_size, 64), 
+                                           nn.Tanh(), 
+                                           nn.Linear(64, 32), 
+                                           nn.Tanh(), 
+                                           nn.Linear(32, self.action_size), 
+                                           nn.Tanh())
+        
+        self.value_layer = nn.Sequential(nn.Linear(state_size, 64), 
+                                         nn.Tanh(), 
+                                         nn.Linear(64, 32), 
+                                         nn.Tanh(), 
+                                         nn.Linear(32, 1))
+                
+        self.action_var = torch.full((self.action_size,), self.action_std*self.action_std)
+        
+    def act(self, state):
+        
+        action_mean = self.policy_layer(state)
+        action_var = self.action_var
+        
+        return action_mean, action_var
+                                    
+    def evaluate(self, state):
+    
+        Qsa = self.value_layer(state)
+        return Qsa
+        
