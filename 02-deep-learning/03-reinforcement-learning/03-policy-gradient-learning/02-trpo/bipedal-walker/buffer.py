812222db81f9a8483254ee5fae76b49353abbbe0
Muhammad Angga Muttaqien <muha.muttaqien@gmail.com>
2020-03-06 08:36:16 +0900

    [RL][feat] Implement TRPO algorithm for bipedal walker env

---
+++ 02-deep-learning/03-reinforcement-learning/03-policy-gradient-learning/02-trpo/bipedal-walker/buffer.py
@@ -0,0 +1,34 @@
+import random
+import numpy as np
+from collections import namedtuple
+
+import torch
+
+is_cuda = torch.cuda.is_available()
+
+if is_cuda: device = torch.device('cuda')
+else: device = torch.device('cpu')
+
+    
+class BasicBuffer(object):
+    
+    def __init__(self, seed=90):
+        """Initialize a BasicBuffer object."""
+        
+        self.experience = namedtuple('Transition', ('state', 'action', 'reward', 'next_state', 'mask'))
+        self.seed = random.seed(seed)
+        
+        self.memory = []
+
+    def add(self, state, action, reward, next_state, mask):
+        """Add a new experience to buffer."""
+            
+        self.memory.append(self.experience(state, action, reward, next_state, mask))
+
+    def sample(self):
+        """Randomly sample a batch of experiences from memory."""
+        return self.experience(*zip(*self.memory))
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
