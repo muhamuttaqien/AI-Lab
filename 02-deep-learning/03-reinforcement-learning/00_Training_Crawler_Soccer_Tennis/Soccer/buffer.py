836c8e9c03955195d4f6718d5f1280420ad15f3e
Muhammad Angga Muttaqien <muha.muttaqien@gmail.com>
2020-03-22 23:31:05 +0900

    [RL][feat] Replace RL repositories

--- 02-deep-learning/03-reinforcement-learning/00_Training_Crawler_Soccer_Tennis/Soccer/buffer.py
+++ 02-deep-learning/03-reinforcement-learning/00_Training_Crawler_Soccer_Tennis/Soccer/buffer.py
@@ -1,8 +1,6 @@
-import os
 import random
 import numpy as np
-import matplotlib.pyplot as plt
-from collections import namedtuple, deque
+from collections import namedtuple
 
 import torch
 
@@ -10,45 +8,45 @@ is_cuda = torch.cuda.is_available()
 
 if is_cuda: device = torch.device('cuda')
 else: device = torch.device('cpu')
-
-
+    
 class ReplayBuffer(object):
     """Fixed-size buffer to store experience tuples."""
     
-    def __init__(self, buffer_size, batch_size, seed):
+    def __init__(self, batch_size, seed):
+        """Initialize a ReplayMemory object."""
         
-        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
-        self.seed = random.seed(seed)
+        self.experience = namedtuple('Experience', field_names=['actor_state', 'critic_state', 'action', 'log_prob', 'reward'])        
+        self.seed = np.random.seed(seed)
         
-        self.memory = deque(maxlen=buffer_size)
-        self.batch_size = batch_size
-        
-    def add(self, state, action, reward, next_state, done):
+        self.memory = []
+
+    def add(self, actor_state, critic_state, action, log_prob, reward):
         """Add a new experience to buffer."""
         
-        self.memory.append(self.experience(state, action, reward, next_state, done))
+        self.memory.append(self.experience(actor_state, critic_state, action, log_prob, reward))
         
-    def sample(self):
-        """Randomly sample a batch of experiences from memory."""
-        experiences = random.sample(self.memory, k=self.batch_size)
+    def get_experiences(self):
+        """Get collected experiences."""
         
-        states = torch.from_numpy(np.vstack([exp.state for exp in experiences if exp is not None])).float()
-        states = states.to(device)
+        actor_states = np.vstack([exp.actor_state for exp in self.memory if exp is not None])
+        critic_states = np.vstack([exp.critic_state for exp in self.memory if exp is not None])
+        actions = np.vstack([exp.action for exp in self.memory if exp is not None])
+        log_probs = np.vstack([exp.log_prob for exp in self.memory if exp is not None])
+        rewards = np.vstack([exp.reward for exp in self.memory if exp is not None])
         
-        actions = torch.from_numpy(np.vstack([exp.action for exp in experiences if exp is not None])).float()
-        actions = actions.to(device)
-        
-        rewards = torch.from_numpy(np.vstack([exp.reward for exp in experiences if exp is not None])).float()
-        rewards = rewards.to(device)
-        
-        next_states = torch.from_numpy(np.vstack([exp.next_state for exp in experiences if exp is not None])).float()
-        next_states = next_states.to(device)
-        
-        dones = torch.from_numpy(np.vstack([exp.done for exp in experiences if exp is not None]).astype(np.uint8)).float()
-        dones = dones.to(device)
-        
-        return (states, actions, rewards, next_states, dones)
+        num_experiences = len(self)
+            
+        return (actor_states, critic_states, actions, log_probs, rewards, num_experiences)
+    
+    def delete(self, index):
+        """Delete experience by index."""
+        del self.memory[index]
+    
+    def reset(self):
+        """Reset all collected experiences from memory."""
+        self.memory = []
         
     def __len__(self):
         """Return the current size of internal memory."""
         return len(self.memory)
+    
--- 02-deep-learning/03-reinforcement-learning/00_Training_Crawler_Soccer_Tennis/Soccer/model.py
+++ 02-deep-learning/03-reinforcement-learning/00_Training_Crawler_Soccer_Tennis/Soccer/model.py
@@ -1,98 +1,69 @@
-import numpy as np
-
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-def hidden_init(layer):
+def layer_init(layer, weight_scale=1.0):
     
-    fan_init = layer.weight.data.size()[0]
-    limit = 1. / np.sqrt(fan_init)
-    return (-limit, limit)
+    nn.init.orthogonal_(layer.weight.data)
+    layer.weight.data.mul_(weight_scale)
+    nn.init.constant_(layer.bias.data, 0)
+    
+    return layer
 
-class PolicyNetwork(nn.Module):
-    """Policy (Actor) Network."""
+class ActorNetwork(nn.Module):
+    """Actor (Policy) Model."""
     
     def __init__(self, state_size, action_size, seed):
-        """Initialize parameters and build model."""
         
-        super(PolicyNetwork, self).__init__()
-        
+        super(ActorNetwork, self).__init__()
+
         self.seed = torch.manual_seed(seed)
         
         self.state_size = state_size
         self.action_size = action_size
-                
-        self.fc1_linear = nn.Linear(self.state_size, 256)
-        self.fc2_linear = nn.Linear(256, 256)
-        self.fc3_linear = nn.Linear(256, self.action_size)
-
-        self.normalizer = nn.BatchNorm1d(256)
         
-        self.reset_parameters()
+        self.fc1_linear = layer_init(nn.Linear(self.state_size, 256))
+        self.fc2_linear = layer_init(nn.Linear(256, 128))
+        self.fc3_linear = layer_init(nn.Linear(128, self.action_size))
         
-    def reset_parameters(self):
-        self.fc1_linear.weight.data.uniform_(*hidden_init(self.fc1_linear))
-        self.fc2_linear.weight.data.uniform_(*hidden_init(self.fc2_linear))
-        self.fc3_linear.weight.data.uniform_(-3e-3, 3e-3)
-
-    def forward(self, states):
+    def forward(self, state, action=None):
         
-        if states.dim() == 1:
-            states = torch.unsqueeze(states, 0)
-        
-        x = F.relu(self.fc1_linear(states))
-        x = self.normalizer(x)
+        x = F.relu(self.fc1_linear(state))
         x = F.relu(self.fc2_linear(x))
-        x = F.tanh(self.fc3_linear(x))
+        action_prob = F.softmax(self.fc3_linear(x), dim=1)
         
-        return x
+        dist = torch.distributions.Categorical(action_prob)
+        
+        if action is None:
+            action = dist.sample()
+            
+        action_log_prob = dist.log_prob(action)
+        dist_entropy = dist.entropy().mean()
 
-class ValueNetwork(nn.Module):
-    """Value (Critic) Network."""
+        return action, action_log_prob, dist_entropy
     
-    def __init__(self, state_size, action_size, seed):
-        """Initialize parameters and build model."""
+class CriticNetwork(nn.Module):
+    """Critic (Value) Model."""
+    
+    def __init__(self, state_size, seed):
         
-        super(ValueNetwork, self).__init__()
-        
+        super(CriticNetwork, self).__init__()
+
         self.seed = torch.manual_seed(seed)
         
         self.state_size = state_size
-        self.action_size = action_size
-                
-        self.fc1_linear = nn.Linear(self.state_size, 256)
-        self.fc2_linear = nn.Linear(256, 256)
-        self.fc3_linear = nn.Linear(256, 1)
+            
+        self.fc1_linear = layer_init(nn.Linear(self.state_size, 256))
+        self.fc2_linear = layer_init(nn.Linear(256, 128))
+        self.fc3_linear = layer_init(nn.Linear(128, 1))
         
-        self.normalizer = nn.BatchNorm1d(256)
+    def forward(self, state):
         
-        self.reset_parameters()
-        
-    def reset_parameters(self):
-        self.fc1_linear.weight.data.uniform_(*hidden_init(self.fc1_linear))
-        self.fc2_linear.weight.data.uniform_(*hidden_init(self.fc2_linear))
-        self.fc3_linear.weight.data.uniform_(-3e-3, 3e-3)
-
-    def forward(self, states, actions):
-        
-        xs = torch.cat((states, actions), dim=1)
-        x = F.relu(self.fc1_linear(xs))
-        x = self.normalizer(x)
+        x = F.relu(self.fc1_linear(state))
         x = F.relu(self.fc2_linear(x))
         Qsa = self.fc3_linear(x)
         
         return Qsa
-
-class ActorCriticNetwork():
-    """Object containing all models required per DDPG agent."""
     
-    def __init__(self, num_agents, state_size, action_size, seed):
-        
-        self.actor = PolicyNetwork(state_size, action_size, seed)
-        self.actor_target = PolicyNetwork(state_size, action_size, seed)
-        
-        critic_input_size = (state_size+action_size) * num_agents
-        self.critic = ValueNetwork(critic_input_size, action_size, seed)
-        self.critic_target = ValueNetwork(critic_input_size, action_size, seed)
